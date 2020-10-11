05aa1a0f8b402176acf8533297f0a0f716a3ace6
Stanislav Prasolov <stanislav.prasolov@cargo.one>
2020-10-12 01:46:51 +0200

    Completer refactoring

--- cli.py
+++ cli.py
@@ -1,35 +1,40 @@
-import re
 import os
-
+import re
 from typing import Iterable
 
-from prompt_toolkit.completion import WordCompleter, CompleteEvent, Completion
+from prompt_toolkit.completion import CompleteEvent, Completion, WordCompleter
 from prompt_toolkit.document import Document
 from prompt_toolkit.shortcuts import CompleteStyle, prompt
 
 from clipboard import copy2clip
 
-DATA_FOLDER = 'data/'
+DATA_FOLDER = 'data'
 
 
 class KaomojiCompleter(WordCompleter):
     KAOMOJI_SUB_REGEX = r'^(\w+\s+)+'
 
-    def __init__(self, *args, **kwargs):
-        super(KaomojiCompleter, self).__init__(*args, **kwargs)
+    def __init__(self, words, *args, **kwargs):
+        self._words = []
+        for word in words:
+            kaomoji = self.parse_kaomoji(word)
+            if kaomoji not in word:
+                print(f"Skipping '{kaomoji}': Failed to correctly parse from '{word}'")
+                continue
+            self._words.append(word)
+
+        super(KaomojiCompleter, self).__init__(self._words)
+
         # `search_history` is a set of last used words
         # TODO: list should be replaced with something like queue of unique values
         # self.search_history = list()
         self.search_history = []
-        self._words = kwargs.get('words') or []
-        self.words = self.get_words_by_search_history
 
-    @classmethod
-    def get_kaomoji(cls, text: str) -> str:
-        return re.sub(cls.KAOMOJI_SUB_REGEX, '', text)
+    def parse_kaomoji(self, text: str) -> str:
+        return re.sub(self.KAOMOJI_SUB_REGEX, '', text)
 
     def get_words_by_search_history(self):
-        """Returns words giving priority to search history"""
+        """Returns words giving priority to recently searched"""
         return self.search_history + [w for w in self._words if w not in self.search_history]
 
     def update_search_history(self, kaomoji):
@@ -42,10 +47,7 @@ class KaomojiCompleter(WordCompleter):
 
     def get_completions(self, document: Document, complete_event: CompleteEvent) -> Iterable[Completion]:
         # Get list of words.
-        words = self.words
-        if callable(words):
-            words = words()
-
+        words = self.get_words_by_search_history()
         word_before_cursor = document.text_before_cursor.lower()
 
         # TODO: If no words were found for search, return an error instead of copying input
@@ -53,20 +55,14 @@ class KaomojiCompleter(WordCompleter):
         for a in words:
             if word_before_cursor in a.lower():
                 display_meta = self.meta_dict.get(a, "")
-                yield Completion(self.get_kaomoji(a), -len(word_before_cursor), display_meta=display_meta)
+                yield Completion(self.parse_kaomoji(a), -len(word_before_cursor), display_meta=display_meta)
 
 
 def main():
     words = []
     for file in os.listdir('data'):
         with open(os.path.join(DATA_FOLDER, file)) as f:
-            for line in f.readlines():
-                line = line.strip()
-                kaomoji = KaomojiCompleter.get_kaomoji(line)
-                if kaomoji not in line:
-                    print(f"Skipping '{kaomoji}': Failed to correctly parse from '{line}'")
-                    continue
-                words.append(line)
+            words.extend([line[:-1] for line in f.readlines()])
 
     kaomoji_completer = KaomojiCompleter(words=words)
 
